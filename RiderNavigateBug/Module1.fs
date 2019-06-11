e8238a0ed32f479a372b2b9d3cd87f29ce630a58
kirichenko <vaskir@gmail.com>
2019-06-11 16:21:31 +0300

    init

---
+++ RiderNavigateBug/Module1.fs
@@ -0,0 +1,19 @@
+module RiderNavigateBug.Extracting
+
+let crl1 = 1
+let crl2 = 1
+let crl3 = 1
+let crl4 = 1
+let crl5 = 1
+let crl6 = 1
+let crl7 = 1
+let crl8 = 1
+let crl9 = 1
+let crl10 = 1
+let crl11 = 1
+let crl12 = 1
+let crl13 = 1
+let crl14 = 1
+let crl15 = 1
+let crl16 = 1
+let crl17 = 1
---
+++ RiderNavigateBug/Program.fs
@@ -0,0 +1,6 @@
+﻿let ``extract CRL distribution points``() = ()
+
+[<EntryPoint>]
+let main _ =
+    printfn "Hello World from F#!"
+    0
